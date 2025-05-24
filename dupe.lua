c65a22db7220d58c6a53ceb931e0967cabcf78cb
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 16:41:28 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -3,13 +3,17 @@ local Window = Library.CreateLib("Dupe Pet by Mangekoy2 | v2.0", "Serpent")
 
 -- Вкладка Dupe Pet
 local DupeTab = Window:NewTab("Dupe Pet")
-local DupeSection = DupeTab:NewSection("DUPE PET")
+local DupeSection = DupeTab:NewSection("Dupe Pet")
+DupeSection:NewLabel("Credits: Made by Mangekoy2 | v2.0") -- Вторая строчка слева
 
 -- Текстовое поле с подсказкой
 local InputField = DupeSection:NewTextBox("", "Write any pet", function(text)
     -- Обработка введенного текста
 end)
 InputField.Instance.Size = UDim2.new(0, 250, 0, 30)
+
+-- Надпись перед кнопкой
+DupeSection:NewLabel("Dupe")
 
 -- Кнопка Dupe
 DupeSection:NewButton("Dupe", "", function()
